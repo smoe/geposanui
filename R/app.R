40cb8e6cbd7285d0f090fbb3b8ac4d3f6e9d2d4e
Elias Projahn <elias@johrpan.de>
2021-10-21 15:57:08 +0200

    Add workaround to include java script dependencies

--- R/app.R
+++ R/app.R
@@ -4,5 +4,13 @@
 #'
 #' @export
 run_app <- function(port = 3464) {
+    # These function calls make the required java scripts available.
+
+    shinyjs::useShinyjs()
+    rclipboard::rclipboardSetup()
+    shinyWidgets::pickerInput("none", choices = NULL)
+
+    # Actually run the app.
+
     shiny::runApp(shiny::shinyApp(ui, server), port = port)
 }
